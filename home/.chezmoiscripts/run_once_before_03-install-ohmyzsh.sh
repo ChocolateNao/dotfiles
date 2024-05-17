be659868812903909e087ad62067ed42d2eebeb6
Andrey Loginov <andrey12q112@gmail.com>
2024-05-17 16:06:42 +0300

    refactor(zsh): rename script

--- home/.chezmoiscripts/run_once_before_03-install-ohmyzsh.sh
+++ home/.chezmoiscripts/run_once_before_03-install-ohmyzsh.sh
@@ -4,5 +4,5 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Installing ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
-	echo ".oh-my-zsh already installed, skipping."
+	echo ".oh-my-zsh already installed, skipping..."
 fi
