f089b33de19c7b9bd61ea1934e86588058828eec
Andrey Loginov <andrey12q112@gmail.com>
2024-05-22 03:00:40 +0300

    feat(ansible): add ability to upgrade flatpaks

--- home/dot_bootstrap/filter_plugins/toml_filter.py
+++ home/dot_bootstrap/filter_plugins/toml_filter.py
@@ -5,8 +5,8 @@ import toml
 class FilterModule(object):
     def filters(self):
         return {
-            'from_toml': from_toml
+            'from_toml': self.from_toml
         }
 
-def from_toml(data):
-    return toml.loads(data)
+    def from_toml(self, data):
+        return toml.loads(data)
